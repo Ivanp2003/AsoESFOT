32d353153eefde8e143729dac9d5e1578d9b2a68
Ivanp2003 <cdandrespanchichavez@gmail.com>
2024-12-16 19:51:20 -0500

    commit

---
+++ asoesfot-Main/accion.js
@@ -0,0 +1,18 @@
+alert ("Bienvenido a la esfot");
+
+/*SUMA CON VAR*/
+var val1=5;
+var val2=10;
+suma= val1+val2;
+
+alert("La suma es: "+suma);
+
+/*SUMA CON LET*/
+let x=11;
+let y=100;
+suma2= x+y;
+
+alert("La segunda suma es:"+suma2);
+
+
+/* */
